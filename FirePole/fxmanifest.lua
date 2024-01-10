8aeff65897b5bf86504b80b6897574e0c57bbcc5
SkyHigh Modifications <93752549+SkyHighModifications@users.noreply.github.com>
2024-01-10 22:22:22 +0000

    Commit Script

--- FirePole/fxmanifest.lua
+++ FirePole/fxmanifest.lua
@@ -1,13 +1,13 @@
 fx_version 'cerulean'
 games { 'gta5' }
 
-author "BKing Development"
+author "BKing Development / Forked SkyHigh Modifications"
 version "1.0.0"
 
 lua54 "yes"
 
 client_scripts {
-    "Config.lua",
+    "config.lua",
     "client.lua",
 }
 
